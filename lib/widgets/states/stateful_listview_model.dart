12b42509ae49d3b9c02926126b7da5ab8eb9d39d
Korppi <3602291+Korppi@users.noreply.github.com>
2022-03-16 21:26:34 +0200

    added loadMoreData method and comments

--- lib/widgets/states/stateful_listview_model.dart
+++ lib/widgets/states/stateful_listview_model.dart
@@ -3,6 +3,11 @@ import 'package:tmdbapp/widgets/states/stateful_listview_state.dart';
 
 class StatefulListviewModel extends StateNotifier<StatefulListviewState> {
   final Future<List<dynamic>> Function({int page}) loadData;
+
+  /// pagination is out of state for simplicity and how app works
+  /// if more functionality comes around then lets move this to state
+  // TODO: double check this page variable and decide if it should stay here
+  int _page = 1;
   StatefulListviewModel(this.loadData)
       : super(const StatefulListviewState.init());
 
@@ -22,6 +27,20 @@ class StatefulListviewModel extends StateNotifier<StatefulListviewState> {
     try {
       var stuff = await loadData();
       state = StatefulListviewState.noError(stuff);
+    } catch (error) {
+      state = StatefulListviewState.error(error.toString());
+    }
+  }
+
+  loadMoreData() async {
+    _page++;
+    var currList =
+        state.maybeWhen(noError: (list) => list, orElse: () => <dynamic>[]);
+    state = StatefulListviewState.loadingMore(currList);
+    try {
+      var stuff = await loadData(page: _page);
+      currList.addAll(stuff);
+      state = StatefulListviewState.noError(currList);
     } catch (error) {
       state = StatefulListviewState.error(error.toString());
     }
